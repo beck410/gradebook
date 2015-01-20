ef981daf1e4ba52c77918a0f578e5e38c1276929
beck410 <beck410@hotmail.com>
2015-01-19 14:31:25 -1200

    created gradebook and added basic functionality

---
+++ Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace gradebook {
+  class Program {
+    static void Main(string[] args) {
+      GradeBook book = new GradeBook();
+      book.AddGrade(91);
+      book.AddGrade(89.1f);
+      book.AddGrade(75);
+
+      GradeStatistics stats = book.ComputeStatistics();
+      Console.WriteLine("Average: " + stats.AverageGrade);
+      Console.WriteLine("Lowest: " + stats.LowestGrade);
+      Console.WriteLine("Highest: " + stats.HighestGrade);
+    }
+  }
+}
